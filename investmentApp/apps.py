ccaf23fe586cd6b64106daf8e9160521b06bb749
barduchi02 <yuri.barduchi@gmail.com>
2022-07-14 14:06:40 -0300

    Funcionalidade de envio de e-mail

--- investmentApp/apps.py
+++ investmentApp/apps.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import AppConfig
+from django.core.mail import send_mail
 
 
 class InvestmentappConfig(AppConfig):
@@ -11,6 +12,7 @@ class InvestmentappConfig(AppConfig):
     name = 'investmentApp'
     hg_brasil_url = 'https://api.hgbrasil.com/finance'
     hg_brasil_access_key = 'f2f1f066'
+    e_mail_app = 'yuri.apptest@gmail.com'
 
     # noinspection PyBroadException
     def ready(self):
@@ -53,3 +55,15 @@ class InvestmentappConfig(AppConfig):
                 cotation.save()
                 tunnel.cotationid = cotation
                 tunnel.save()
+
+                # Verificar se um e-mail será enviado, já que uma nova cotação foi obtida
+                bo_venda = tunnel.max_price < cotation.price
+                bo_compra = tunnel.min_price > cotation.price
+                if bo_venda or bo_compra:
+                    subject = 'Negociação recomendada para ações da ' + tunnel.assetid.description
+                    message = 'Segundo o túnel configurado por você na aplicação "Investment Helper", recomenda-se '
+                    message += 'vender' if bo_venda else 'comprar'
+                    message += ' ações ' + tunnel.assetid.code + ' - ' + tunnel.assetid.description + ' pelo valor de '
+                    message += cotation.currency + ' ' + str(cotation.price) + '.'
+
+                    send_mail(subject, message, self.e_mail_app, [tunnel.userid.email], fail_silently=False)
